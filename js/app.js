e7eb835707fdb22aad1f6c1a9791015ccdefe2cf
Zillah Chen <zillah_c@hotmail.com>
2015-01-28 11:41:47 -0800

    make functions

--- js/app.js
+++ js/app.js
@@ -1,20 +1,33 @@
 $(function(){
 
+   
+
+  // function moveRight (){
    $( "#right" ).click(function() {
     console.log("right clicked")
     $( "#spaceship" ).animate({ "left": "+=50px" }, "slow" );
     });
-   
+  // }
+
+  // function moveLeft (){
   $( "#left" ).click(function(){
     console.log("left clicked")
     $( "#spaceship" ).animate({ "left": "-=50px" }, "slow" );
     });
+  // }
+  // function moveUp(){
   $( "#up" ).click(function(){
     console.log("up clicked")
     $( "#spaceship" ).animate({ "top": "-=50px" }, "slow" );
     });
+  // }
+
+  // function moveDown(){
   $( "#down" ).click(function(){
     console.log("down clicked")
     $( "#spaceship" ).animate({ "top": "+=50px" }, "slow" );
     });
+  // }
+
+  
 });
