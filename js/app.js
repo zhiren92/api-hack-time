218743e8425002b0111d730025841447a2d4cb3e
Matthew Reddick <matthewjreddick@gmail.com>
2015-01-28 11:58:40 -0800

    make moves work on voice

--- js/app.js
+++ js/app.js
@@ -1,33 +1,40 @@
-$(function(){
+// $(function(){
 
    
 
-  // function moveRight (){
-   $( "#right" ).click(function() {
-    console.log("right clicked")
-    $( "#spaceship" ).animate({ "left": "+=50px" }, "slow" );
-    });
-  // }
+//   // function moveRight (){
+//    $( "#right" ).click(moveRight);
+//   // }
 
-  // function moveLeft (){
-  $( "#left" ).click(function(){
-    console.log("left clicked")
-    $( "#spaceship" ).animate({ "left": "-=50px" }, "slow" );
-    });
-  // }
-  // function moveUp(){
-  $( "#up" ).click(function(){
-    console.log("up clicked")
-    $( "#spaceship" ).animate({ "top": "-=50px" }, "slow" );
-    });
-  // }
+//   // function moveLeft (){
+//   $( "#left" ).click(moveLeft);
+//   // }
+//   // function moveUp(){
+//   $( "#up" ).click(moveUp);
+//   // }
 
-  // function moveDown(){
-  $( "#down" ).click(function(){
-    console.log("down clicked")
-    $( "#spaceship" ).animate({ "top": "+=50px" }, "slow" );
-    });
-  // }
+//   // function moveDown(){
+//   $( "#down" ).click(moveDown);
+//   // }
 
+//   function moveRight() {
+//   	console.log("right clicked")
+//     $( "#spaceship" ).animate({ "left": "+=50px" }, "slow" );
+//   }
   
-});
+//   function moveLeft() {
+//     console.log("left clicked")
+//     $( "#spaceship" ).animate({ "left": "-=50px" }, "slow" );
+//   }
+
+//   function moveUp(){
+//     console.log("up clicked")
+//     $( "#spaceship" ).animate({ "top": "-=50px" }, "slow" );
+//   }
+
+//   function moveDown(){
+//     console.log("down clicked")
+//     $( "#spaceship" ).animate({ "top": "+=50px" }, "slow" );
+//   }
+  
+// });
