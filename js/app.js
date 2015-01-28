e5261cb29bf36c29745b6fa7c45231ff4bb7f592
Zillah Chen <zillah_c@hotmail.com>
2015-01-28 11:27:34 -0800

    spaceship moves left and right

--- js/app.js
+++ js/app.js
@@ -1,12 +1,20 @@
 $(function(){
 
- $( "#right" ).click(function() {
-  console.log("right clicked")
-  $( "#spaceship" ).animate({ "left": "+=50px" }, "slow" );
-  });
- 
-$( "#left" ).click(function(){
-  console.log("left clicked")
-  $( "#spaceship" ).animate({ "left": "-=50px" }, "slow" );
-  });
+   $( "#right" ).click(function() {
+    console.log("right clicked")
+    $( "#spaceship" ).animate({ "left": "+=50px" }, "slow" );
+    });
+   
+  $( "#left" ).click(function(){
+    console.log("left clicked")
+    $( "#spaceship" ).animate({ "left": "-=50px" }, "slow" );
+    });
+  $( "#up" ).click(function(){
+    console.log("up clicked")
+    $( "#spaceship" ).animate({ "up": "+=50px" }, "slow" );
+    });
+  $( "#down" ).click(function(){
+    console.log("down clicked")
+    $( "#spaceship" ).animate({ "down": "-=50px" }, "slow" );
+    });
 });
